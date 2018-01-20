7e2d402382ea91f4bc81a2ba026a74747cd67815
alechp <me@alechp.com>
2018-01-19 16:17:42 -0800

    Updated repospace name to a path variable

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,7 +3,7 @@
  * @Date:   2018-01-19T16:05:25-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-19T16:12:05-08:00
+ * @Last modified time: 2018-01-19T16:17:34-08:00
  */
 const path = require("path");
 const { create } = require(path.join(__dirname, "../src/api.js"));
@@ -13,6 +13,6 @@ test("Repospace is created", () => {
     "https://github.com/servexyz/kisoro",
     "https://github.com/alechp/bash"
   ];
-  let repospace = ".sandbox";
+  let repospace = path.join(__dirname, ".sandbox");
   create(repos, repospace);
 });
