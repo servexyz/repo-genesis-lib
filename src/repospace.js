23f080884e413b22c6a196e352dd8b206acdca91
alechp <me@alechp.com>
2018-01-19 17:25:42 -0800

    Finished migrating to prototype pattern
    * Removed cruft
    * Moving to global var scoped within IFFE
    * Currently trying to figure out why this.respace = isn't being set... Telling me undefined. Is "this" undefined ?

---
+++ src/repospace.js
@@ -0,0 +1,91 @@
+/**
+ * @Author: Alec Hale-Pletka <alechp>
+ * @Date:   2018-01-17T17:39:25-08:00
+ * @Email:  alec@bubblegum.academy
+ * @Last modified by:   alechp
+ * @Last modified time: 2018-01-19T17:22:33-08:00
+ */
+(function() {
+  "use strict";
+  const Promise = require("bluebird");
+  const chalk = require("chalk");
+  const log = console.log;
+  const path = require("path");
+  const fs = require("fs-extra");
+  const git = require("simple-git");
+
+  function Repospace(repospacePath, hiddenReposPath) {
+    log(`repospacePath: ${chalk.yellow(repospacePath)}`);
+    log(`hiddenReposPath: ${chalk.yellow(hiddenReposPath)}`);
+    this.respace = repospacePath;
+    this.respaceRepos = hiddenReposPath;
+  }
+
+  Repospace.prototype.createDirectories = async function() {
+    chalk.yellow(`Inside createDirectories`);
+    try {
+      await fs.ensureDir(this.respace);
+      await fs.ensureDir(this.respaceRepos);
+      log(
+        `Created: \n ${chalk.blue(this.respace)} \n ${chalk.blue(
+          this.respaceRepos
+        )}`
+      );
+    } catch (err) {
+      log(`Failed to create directories. \n ${chalk.red(err)}`);
+    }
+  };
+
+  Repospace.prototype.getAuthenticatedRemoteStringFromRepo = function(repo) {
+    return new String(
+      `https://${process.env.GIT_USER}:${process.env.GIT_PASS}@${repo}`
+    );
+  };
+
+  Repospace.prototype.cloneFactory = function(repos) {
+    chalk.yellow(`Inside cloneFactory`);
+    let clonedRepos = [];
+    let failedRepos = [];
+    repos.map(repo => {
+      let remote = Repospace.getAuthenticatedRemoteStringFromRepo(repo);
+      git()
+        .clone(remote)
+        .then(() => {
+          log(`This ${chalk.blue(repo)} has been pushed to clonedRepos`);
+          clonedRepos.push(repo);
+        })
+        .catch(err => {
+          log(`${chalk.red(err)}`);
+          failedRepos.push(repo);
+        });
+    });
+    log(`clonedRepos: \n ${chalk.red(clonedRepos.toString())}`);
+    log(`failedRepos: \n ${chalk.blue(failedRepos.toString())}`);
+    this.clonedRepos = clonedRepos;
+    return clonedRepos;
+  };
+
+  Repospace.prototype.symlinkFactory = async function(
+    repos = this.clonedRepos
+  ) {
+    chalk.yellow(`Inside symlinkFactory`);
+    let createdSymlinks = [];
+    let failedSymlinks = [];
+    for (let repo in clonedRepos) {
+      let source = path.join(__dirname, repo, hiddenDirectory);
+      let target = path.join(__dirname, repo);
+      try {
+        await fs.ensureSymlink(source, target);
+      } catch (err) {
+        failedSymlinks.push(target);
+        log(`Failed to create symlink \n ${chalk.red(err)}`);
+      }
+      createdSymlinks.push(target);
+    }
+    return createdSymlinks;
+  };
+  // 1.0
+  // TODO: function symlinkLayoutFactory(symlinks) {}
+
+  module.exports = Repospace;
+})();
---
+++ tests/repospace.test.js
@@ -0,0 +1,27 @@
+/**
+ * @Author: Alec Hale-Pletka <alechp>
+ * @Date:   2018-01-19T16:05:25-08:00
+ * @Email:  alec@bubblegum.academy
+ * @Last modified by:   alechp
+ * @Last modified time: 2018-01-19T17:21:27-08:00
+ */
+
+require("dotenv").config();
+const path = require("path");
+const repospace = require(path.join(__dirname, "../src/repospace.js"));
+
+test("Repospace is created", () => {
+  let repos = [
+    "https://github.com/servexyz/kisoro",
+    "https://github.com/alechp/bash"
+  ];
+
+  let respace = ".sandbox";
+  let respacePath = path.join(__dirname, respace);
+  let hiddenReposPath = path.join(__dirname, respace, ".repos");
+
+  repospace(respacePath, hiddenReposPath)
+    .createDirectories()
+    .cloneFactory(repos)
+    .symlinkFactory();
+});
