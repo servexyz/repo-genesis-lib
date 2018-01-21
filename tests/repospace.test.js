1eccaa4f284abf2fa8cafc74794e5500e479e5fa
alechp <me@alechp.com>
2018-01-21 11:29:41 -0800

    Marshalled to ES6 classes & ES7 async await struct instead of prototypes

--- tests/repospace.test.js
+++ tests/repospace.test.js
@@ -3,25 +3,44 @@
  * @Date:   2018-01-19T16:05:25-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-19T17:32:57-08:00
+ * @Last modified time: 2018-01-21T11:11:13-08:00
  */
 
 require("dotenv").config();
 const path = require("path");
 const repospace = require(path.join(__dirname, "../src/repospace.js"));
 
+async function instantiateRepospace(repos, respacePath, reposPath) {
+  let r = new Repospace(respacePath, reposPath);
+  try {
+    let directories = await r.createDirectories();
+    let repositories = await r.cloneFactory(repos);
+    return true;
+  } catch (err) {
+    log(
+      `Failed to createDirectories or cloneRepositories. \n ${chalk.red(err)}`
+    );
+    return false;
+  }
+}
+
 test("Repospace is created", () => {
+  // "https://github.com/servexyz/kisoro",
+  // "https://github.com/alechp/bash"
   let repos = [
-    "https://github.com/servexyz/kisoro",
-    "https://github.com/alechp/bash"
+    {
+      acct: "servexyz",
+      repo: "kisoro"
+    },
+    {
+      acct: "alechp",
+      repo: "bash"
+    }
   ];
 
-  let respace = ".sandbox";
-  let respacePath = path.join(__dirname, respace);
-  let hiddenReposPath = path.join(__dirname, respace, ".repos");
-
-  repospace(respacePath, hiddenReposPath)
-    .createDirectories()
-    .cloneFactory(repos)
-    .symlinkFactory();
+  let respaceName = ".sandbox";
+  let respacePath = path.join(__dirname, respaceName);
+  let reposPath = path.join(__dirname, respaceName, ".repos");
+  let attempt = instantiateRepos(repos, respacePath, reposPath);
+  expect(attempt).toBe(true);
 });
