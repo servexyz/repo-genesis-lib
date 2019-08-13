0d9c0d6c7d931d6ec6e16783a9eb14816bba9923
alechp <me@alechp.com>
2019-08-13 15:47:33 -0700

    Confirmed parse and rgGen work together

--- tests/generate.test.js
+++ tests/generate.test.js
@@ -3,19 +3,30 @@ import { rgGen } from "../src/generate";
 import test from "ava";
 import chalk from "chalk";
 import path from "path";
-import { parseConfig, readConfig } from "../src/parse";
+import { parse, parseConfig, readConfig } from "../src/parse";
 import { printMirror } from "tacker";
 
 const configFile = path.resolve(__dirname, "../", "sandbox", ".repogen.json");
 test.before(t => {
   process.env.rgAuthHost = "alechp";
+  delete process.env.rgAuthHost;
 });
 
-//* Should work, just don't want to run everytime
+//* Reasons for skipping some/all generate tests:
+// --------------------------------
+//* Slow to run everytime
+//* "alechp" provider doesn't work on travis
+
 test.skip(`${chalk.blue("rgGen(")}${chalk.cyan("all")}${chalk.blue(
   ")"
 )} :: returns true `, async t => {
   let config = await readConfig(configFile);
   let parsedConfig = await parseConfig(config);
+  t.true(await rgGen(parsedConfig, "all"));
+});
+test.skip(`${chalk.blue("rgGen(")}${chalk.cyan("all")}${chalk.blue(
+  ")"
+)} works with ${chalk.underline.grey("parse(null)")}`, async t => {
+  let parsedConfig = await parse();
   t.true(await rgGen(parsedConfig, "all"));
 });
